c3dcc4b443ece03027733bcb4cf645e1a52c7ceb
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-09-07 13:42:56 +0000

    setup json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 import Header from "components/header"
 import Footer from "components/footer"
 
 const Home: NextPage = () => {
   return (
-      <div>
+      <>
           <Header />
           <Footer />
-      </div>
+      </>
   )
 }
 
