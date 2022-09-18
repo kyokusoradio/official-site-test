36430eb4041f169e877bc1934a269d91d7cb9d9a
旭操ラジオ <kyokuso.radio0612@gmail.com>
2022-09-18 06:48:38 +0000

    setup toppage and title

---
+++ components/top-page.jsx
@@ -0,0 +1,16 @@
+import styles from "styles/top-page.module.css"
+
+export default function Toppage() {
+  return (
+    <section className={styles.heroContainer}>
+      <h1 className={styles.title}>
+        <span className={styles.titleTop}>Kyokuso</span><br></br>
+        <span className={styles.titleBottom}>Radio</span>
+      </h1>
+      <p className={styles.subtitle}>「そういえば」な話題をゆるっとお届けするポッドキャスト</p>
+      <div className={styles.topAnimation}>
+        
+      </div>
+    </section>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,15 @@ import Image from 'next/image'
 import styles from 'styles/Home.module.css'
 
 import Header from "components/header"
+import Toppage from "components/top-page"
 import Footer from "components/footer"
 
 const Home: NextPage = () => {
   return (
       <>
-          <Header />
-          <Footer />
+      <Header />
+      <Toppage />
+      <Footer />
       </>
   )
 }
